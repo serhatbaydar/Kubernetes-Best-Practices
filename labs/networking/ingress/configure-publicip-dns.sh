d6a9c658ec6a32cf0fd2dbf83638bd9273a11a7a
serhatbaydar <39600256+serhatbaydar@users.noreply.github.com>
2019-02-21 14:46:12 +0300

    Update configure-publicip-dns.sh

--- labs/networking/ingress/configure-publicip-dns.sh
+++ labs/networking/ingress/configure-publicip-dns.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Public IP address
-IP="137.117.57.130"
+IP="13.82.109.221"
 
 # Name to associate with public IP address
-DNSNAME=servicetracker
+DNSNAME=serbay
 
 # Get the resource-id of the public ip
 PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
